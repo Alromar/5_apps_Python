bd7737378b57e581c692d24731aac0a5ba36dc87
Alromar <alex-markus@outlook.com>
2025-04-18 12:18:55 +0200

    sectie 2 6

---
+++ download_multiple_files.py
@@ -0,0 +1,7 @@
+import time
+import requests
+from multiprocessing import cpu_count
+from multiprocessing.pool import ThreadPool
+
+urls = ("https://img-c.udemycdn.com/course/480x270/5333182_0cd1.jpg",
+        )
